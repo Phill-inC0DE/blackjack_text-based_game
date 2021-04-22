33e7bbb9e956134660418409929b429dbbbad93e
Philip Worswick <prworswick@live.co.uk>
2021-04-22 17:38:18 +0100

    stuck at error message Typer Error line 25

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from script import full_deck
+from build_deck import full_deck
 import random
 # card set no. 4, 13, 52
 
-full_deck_dict = full_deck
-full_deck_list = [key + " - Value: " + str(value) for key, value in full_deck_dict.items()]
-shuffle_deck = [random.randint(0, len(full_deck_list)-1), random.randint(0, len(full_deck_list)-1)]
+dealer_cards_value = {"Dealer": 0}
+player_cards_value = {}
+ask_name = input("What is your name? ")
+player_cards_value[ask_name] = 0
 
-print(full_deck_list[shuffle_deck[0]]," | ", full_deck_list[shuffle_deck[1]])
+amount_in_pot = 0
+money_pot = "£{amount}".format(amount=amount_in_pot)
+player_turn = 0
+dealer_turn = 0
+player_cards = {}
+dealer_cards = {}
+
+key_deck_list = [key for key in full_deck.keys()]
+value_deck_list = [value for value in full_deck.values()]
+
+while True:
+    four_random = random.sample(range(0, 52), 4)
+    player_cards[key_deck_list[four_random[0]]] = value_deck_list[four_random[0]]
+    player_cards[key_deck_list[four_random[1]]] = value_deck_list[four_random[1]]
+    player_value = player_cards.values()
+    player_total_value = sum(player_value)
+    player_cards_value[ask_name] = player_total_value
+    if len(player_cards) == 2:
+        dealer_cards[key_deck_list[four_random[2]]] = value_deck_list[four_random[2]]
+        dealer_cards[key_deck_list[four_random[3]]] = value_deck_list[four_random[3]]
+        dealer_values = dealer_cards.values()
+        dealer_total_value = sum(dealer_values)
+        dealer_cards_value["Dealer"] = dealer_total_value
+    elif len(dealer_cards) == 2 and len(player_cards) == 2:
+        print("Dealer shows: " + str(dealer_cards) + " total value: {}".format(dealer_cards_value.get("Dealer")))
+        prompt = input("What would you like to do? Hit or Fold. ")
