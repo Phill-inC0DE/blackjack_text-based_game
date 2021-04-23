507a3c9e853a84524e2429b799bff75b69a4ce01
Philip Worswick <prworswick@live.co.uk>
2021-04-23 19:47:05 +0100

    days end changes 23.4.21

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from build_deck import full_deck
+from sys import exit
 import random
 import time
+
 # card set no. 4, 13, 52
 
 dealer_cards_value = {"Dealer": 0}
@@ -11,68 +13,87 @@ player_cards_value[ask_name] = 0
 amount_in_pot = 1000
 money_pot = "£{amount}".format(amount=amount_in_pot)
 bet = 0
-player_turn = 0
-dealer_turn = 0
-player_cards = {}
-dealer_cards = {}
 
-
-print(
-    "Hi {name}! Welcome to Casino Royale!\n"
-    "New comers get £1000 to play with.".format(name=ask_name)
-    )
 
 while True:
-    confirm_prompt = input("Would you like to continue? Y/N\n")
-    if confirm_prompt.upper() == "Y" and type(confirm_prompt) != int:
-        bet_prompt = input("How much would you like to bet? Current amount £{0}\n".format(amount_in_pot))
-        try:
-            int(bet_prompt)
-            if int(bet_prompt) in range(1, amount_in_pot):
-                bet_confirm_prompt = input("You bet £{0}, are you happy to continue? Y/N:\n".format(bet_prompt))
-                if bet_confirm_prompt.upper() == "Y" and type(confirm_prompt) != int:
-                    amount_in_pot -= int(bet_prompt)
-                    bet += int(bet_prompt)
-                    print("You have now placed a bet for £{0}, and have £{1} left remaining in your pot.".format(bet, amount_in_pot))
-                    begin_game = input("Ready to play? Y/N:\n")
-                    if begin_game.upper() == "Y" and type(begin_game) != int:
-                        time.sleep(1)
-                        shuffle = print("\nShuffling...")
-                        time.sleep(3)
-                        dealing = print("\nNow dealing cards...")
-                        time.sleep(3)
-                        break
-                    else:
-                        amount_in_pot += int(bet)
-                        bet -= int(bet_prompt)
-                        continue
-            else:
-                print("You do not have enough.")
+    player_turn = 0
+    dealer_turn = 0
+    player_cards = {}
+    dealer_cards = {}
+
+    print(
+        "Hi {name}! Welcome to Casino Royale!\n"
+        "New comers get £1000 to play with.".format(name=ask_name)
+        )
+
+    while True:
+        confirm_prompt = input("Would you like to continue? Y/N\n")
+        if confirm_prompt.upper() == "Y" and type(confirm_prompt) != int:
+            bet_prompt = input("How much would you like to bet? Current amount £{0}\n".format(amount_in_pot))
+            try:
+                int(bet_prompt)
+                if int(bet_prompt) in range(1, amount_in_pot):
+                    bet_confirm_prompt = input("You bet £{0}, are you happy to continue? Y/N:\n".format(bet_prompt))
+                    if bet_confirm_prompt.upper() == "Y" and type(confirm_prompt) != int:
+                        amount_in_pot -= int(bet_prompt)
+                        bet += int(bet_prompt)
+                        print("You have now placed a bet for £{0}, and have £{1} left remaining in your pot.".format(bet, amount_in_pot))
+                        begin_game = input("Ready to play? Y/N:\n")
+                        if begin_game.upper() == "Y" and type(begin_game) != int:
+                            time.sleep(1)
+                            shuffle = print("\nShuffling...")
+                            time.sleep(3)
+                            dealing = print("\nNow dealing cards...")
+                            time.sleep(3)
+                            break
+                        else:
+                            amount_in_pot += int(bet)
+                            bet -= int(bet_prompt)
+                            continue
+                else:
+                    print("You do not have enough.")
+                    continue
+            except ValueError:
+                print("You need to place the right bet amount.")
                 continue
-        except ValueError:
-            print("You need to place the right bet amount.")
-            continue
+    
+    new_dict = {}
+    while True:
+        new_dict.clear()
+        key_deck_list = [key for key in full_deck.keys()]
+        value_deck_list = [value for value in full_deck.values()]
 
-key_deck_list = [key for key in full_deck.keys()]
-value_deck_list = [value for value in full_deck.values()]
+        four_random = random.sample(range(0, len(full_deck)), 4)
+        player_cards[key_deck_list[four_random[0]]] = value_deck_list[four_random[0]]
+        player_cards[key_deck_list[four_random[1]]] = value_deck_list[four_random[1]]
+        player_cards_lst = list(player_cards.values())
+        player_cards_value[ask_name] = sum(player_cards_lst)
+        player_turn += sum(player_cards_lst)
+    
+        for key, value in full_deck.items():
+            if key not in list(player_cards.keys()):
+                new_dict[key] = value
 
-four_random = random.sample(range(0, 52), 4)
-player_cards[key_deck_list[four_random[0]]] = value_deck_list[four_random[0]]
-player_cards[key_deck_list[four_random[1]]] = value_deck_list[four_random[1]]
-player_cards_lst = list(player_cards.values())
-player_cards_value[ask_name] = sum(player_cards_lst)
+        print(
+            "Your cards:" + str(player_cards) + "\n" \
+            "Total value for Player: " + str(player_cards_value)
+            )
 
-print(
-    "Your cards:" + str(player_cards) + "\n" \
-    "Total value for Player: " + str(player_cards_value)
-    )
-
-time.sleep(3)
-print("\nNow wait for the dealer.")
-time.sleep(3)
-dealer_cards[key_deck_list[four_random[2]]] = value_deck_list[four_random[2]]
-dealer_cards[key_deck_list[four_random[3]]] = value_deck_list[four_random[3]]
-dealer_cards_lst = list(dealer_cards.values())
-dealer_cards_value["Dealer"] = sum(dealer_cards_lst)
-print("\nDealer shows: " + str(dealer_cards) + " total value: {}".format(dealer_cards_value.get("Dealer")))
-prompt = input("\nWhat would you like to do? Hit or Stay.\n")
+        time.sleep(3)
+        print("\nNow wait for the dealer.")
+        time.sleep(3)
+        dealer_cards[key_deck_list[four_random[2]]] = value_deck_list[four_random[2]]
+        dealer_cards[key_deck_list[four_random[3]]] = value_deck_list[four_random[3]]
+        dealer_cards_lst = list(dealer_cards.values())
+        dealer_cards_value["Dealer"] = sum(dealer_cards_lst)
+        dealer_turn += sum(dealer_cards_lst)
+        print("Dealer shows: " + str(dealer_cards) + " Total value: {}".format(dealer_cards_value.get("Dealer")))
+        
+        new_dict2 = {}
+        for key, value in new_dict.items():
+            if key not in list(dealer_cards.keys()):
+                new_dict2[key] = value
+        
+        turn_prompt = input("\nWhat would you like to do? Hit or Stay. H/S:\n")
+        if turn_prompt.upper() == "H" and type(turn_prompt) != int:
+            pass
